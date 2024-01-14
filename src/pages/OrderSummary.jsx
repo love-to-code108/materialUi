a3da536133efbc1eec5f126643872dafe719f481
love-to-code108 <love.to.code108@gmail.com>
2024-01-14 23:26:48 +0530

    learned how to go to other pages by calling functions

--- src/pages/OrderSummary.jsx
+++ src/pages/OrderSummary.jsx
@@ -1,11 +1,20 @@
 import { useNavigate } from "react-router-dom"
 
 export const OrderSummary = () => {
+
+  
+    const navigate = useNavigate();
+
+
+  
+
+
   return (
     <div>
       <p>Your Order has been confirmed</p>
       <button onClick={() => {
-        const 
+        navigate("/");
+
       }}>Go Back to Home Page</button>
     </div>
   )
